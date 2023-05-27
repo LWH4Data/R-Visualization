b3acd1e078d5ab0356db9481d73d22cd2ecf435f
LWH4Data <132273363+LWH4Data@users.noreply.github.com>
2023-05-27 19:10:20 +0900

    Add files via upload
    How to manipulate legend in R.
    
    I think that using R Cookbook is better than my code. Here are links for visualization and ggplot2's legend.
    
    1. ggplot2::legend CookBook 
    >> http://www.cookbook-r.com/Graphs/Legends_(ggplot2)/#modifying-the-text-of-legend-titles-and-labels
    
    2. R Graphics CookBook
    >> https://r-graphics.org/recipe-legend-label-text
    <$10.8 Changing the Labels in a Legend>

---
+++ legend.manipulation.R
@@ -0,0 +1,50 @@
+library(tidyr)
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+
+#data
+W <- c(3, 2, 1, 1, 4)
+L <- c(5, 2, 4, 2, 3)
+H <- c(7, 4, 5, 3, 6)
+
+#make dataframe and set "id" coulunm
+Vdum <- data.frame(W, L, H)
+Vdum$id <- c(1:5)
+Vdum
+
+#melt dataframe "Vdum" to "Vdum1". The criterion is "id" colunm.
+Vdum1 <- melt(Vdum, id.vars = "id", variable.name = "Group", value.name = "libido")
+Vdum1$id <- c(1:15)
+Vdum1
+
+#visualization each group in "Vdum1". 
+p <- ggplot(data = Vdum1, aes(x = id, y = libido)) +
+  geom_point(aes(shape = Group, size = 3, color = Group)) +
+  scale_color_manual("Group", values = c("W" = "yellow", "L" = "green", "H" = "blue"))
+
+p
+
+#Calculate the mean of each group. Then make it as dataframe.
+MV <- Vdum1 %>% 
+  group_by(Group) %>% 
+  summarize(mean_libido = mean(libido)) %>% 
+  as.data.frame
+MV
+
+#ggplot2::geom_segment.
+#We made a scatterplot whose name is "p" before. We add each mean of groups as horizental line to the scatterplot "p".
+pm <- p + geom_line(aes(y = mean(libido))) +
+  geom_segment(aes(x = 1, xend = 5, y = 2.2, yend = 2.2), color='yellow', size = 1.2) +
+  geom_segment(aes(x = 6, xend = 10, y = 3.2, yend = 3.2), color = 'green', size = 1.2) +
+  geom_segment(aes(x = 11, xend = 15, y = 5, yend = 5), color = 'blue', size = 1.2)
+pm
+
+#legend manipulate
+pm
+pm + scale_shape_discrete(name = "복용", 
+                          breaks = c("W", "L", "H"),
+                          labels = c("위약", "저용량", "고용량")) +
+  scale_color_manual(values = c("yellow", "green", "blue"), name = "복용", 
+                     breaks = c("W", "L", "H"),
+                     labels = c("위약", "저용량", "고용량"))
