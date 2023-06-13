96dd3e7045f68bfce16b6704873b8a61e146d64a
LWH4Data <132273363+LWH4Data@users.noreply.github.com>
2023-06-13 16:57:13 +0900

    Update ggplot2.ggplot(aes(group)).R

--- ggplot2.ggplot(aes(group)).R
+++ ggplot2.ggplot(aes(group)).R
@@ -3,8 +3,9 @@ library(ggplot2)
 #set data
 gender <- gl(2, 24, labels = c("Female", "Male"))
 alcohol <- gl(3, 8, 48, labels = c("None", "2 Pints", "4 Pints"))
-atrractiveness <- c(65, 70, 60, 60, 60, 55, 60, 55, 70, 65, 60, 70, 65, 60, 60, 50, 55, 65, 70, 55,
+attractiveness <- c(65, 70, 60, 60, 60, 55, 60, 55, 70, 65, 60, 70, 65, 60, 60, 50, 55, 65, 70, 55,
                     55, 60, 50, 50, 50, 55, 80, 65, 70, 75, 75, 65, 45, 60, 85, 65, 70, 70, 80, 60, 30, 30, 30, 55, 35, 20, 45, 40)
+gogglesData <- data.frame(gender, alcohol, attractiveness)
 
 #visualize multi factor variables mean.(ggplot(aes(group = variable that we need)))
 ggplot(data = gogglesData, aes(x = alcohol, y = attractiveness, color = gender, group = gender)) +
